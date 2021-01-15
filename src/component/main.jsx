f82d54626d8ead5ce6ce9574e51cd1242391fb27
unknown <xxspanzie@gmail.com>
2021-01-15 15:53:41 +0700

    Mengubah beberapa style

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -60,7 +60,7 @@ class main extends Component {
                     </div>
                     <div className="col-md-6">
                         <div className="row product">
-                            <div className="img col-lg-6" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1601589689349-50bc966e89fc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")` }}>
+                            <div className="img col-lg-6" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1578289367009-faa2c72ba3db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")` }}>
                             </div>
                             <div className="text col-lg-6 d-flex align-items-center">
                             <div className="desk">
